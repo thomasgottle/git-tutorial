0b62fe14b733183810854f2d4525fc9712638e11
thomasgottle <tgoettle@icloud.com>
2023-05-10 22:14:20 +0200

    fixed CMakeLists.txt file because of an waring | modified the main.c function according to the desired requirements

--- src/main.c
+++ src/main.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
+#include "espl_lib.h"
+
 int main()
 {
-    printf("Hello ESPL");
+    unsigned int number;
+    int choice;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    char* text = num_to_words(number);
+
+    printf("The functions output is: %s \n", text);
+
+
+    do{
+        printf("Enter '1' to run another number or '0' to exit: ");
+        scanf("%d", &choice);
+
+        if (choice == 1)
+        {
+            printf("Running the program again ... \n");
+            printf("Enter an integer: ");
+            scanf("%d", &number);
+
+            char* text = num_to_words(number);
+
+            printf("The functions output is: %s \n", text);
+        }
+        else if (choice == 0)
+        {
+            printf("Exiting the program. \n");
+        }
+        else
+        {
+            printf("Invalid choice. Please try again. \n");
+        }
+    } while (choice != 0);
+    
     return 0;
 }
